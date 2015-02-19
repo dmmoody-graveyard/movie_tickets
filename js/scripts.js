0039cd56a83847e6be4a40206edf7a652f56662b
Duane M. Moody <dmmoody@gmail.com>
2015-02-18 16:08:59 -0800

    add list of added movies

--- js/scripts.js
+++ js/scripts.js
@@ -18,5 +18,17 @@ var Pricing = {
 };
 
 $(document).ready(function() {
+  $('form#new-movie').submit(function(event) {
+    event.preventDefault();
 
+    var movieName = $('input#movie-name').val();
+    var releaseDate = $('input#release-date').val();
+
+    var newMovie = Object.create(Movie);
+    newMovie.name = movieName;
+
+    $('ul#show-movies').append("<li><span class='movie'>" + newMovie.name + "</span></li>")
+
+    $('input#movie-name').val("");
+  });
 });
